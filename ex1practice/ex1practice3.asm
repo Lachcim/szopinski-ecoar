1cfe56b805a56c68fadae3fdb677ebb821391248
Michał Szopiński <mszopinski@mssnt.pl>
2020-11-14 19:55:17 +0100

    Finish task 3

--- ex1practice/ex1practice3.asm
+++ ex1practice/ex1practice3.asm
@@ -12,8 +12,22 @@ buflen: .word 255
         
         li      $t0, -1             # forward string iterator, start before first char
         lbu     $t1, buflen         # reverse string iterator, start at terminator
-        jal incfor                  # make iterators point to first digits
-        jal increv
+        jal     incfor              # make iterators point to first digits
+        jal     increv
+        
+iterst: bge     $t0, $t1, stop      # iterate until the iterators pass each other
+
+        sb      $t3 buf($t0)        # swap digits, $t2 and $t3 are dereferenced iterators
+        sb      $t2 buf($t1)
+        
+        jal     incfor              # increment iterators and reiterate
+        jal     increv
+        j       iterst
+
+stop:   li      $v0, 4              # print the rearranged string
+        syscall
+        li      $v0, 10             # exit program
+        syscall
         
 # increment the forward iterator
 incfor: addiu   $t0, $t0, 1         # increment the iterator    
