91bceefeca3afa4a3e58d34be702b56c3c21b3bf
Michał Szopiński <mszopinski@mssnt.pl>
2020-12-17 20:03:09 +0100

    Add BFS

--- markerfinder/chunkfinder.asm
+++ markerfinder/chunkfinder.asm
@@ -42,7 +42,36 @@ explore_chunk:
         mflo    $a1        
         jal     queue_push          # add initial position to BFS queue
         
-        # todo
+exloop: jal     queue_pop           # pop next pixel from queue
+
+        mul     $t0, $v1, 320       # calculate the pixel's index in buffer
+        add     $t0, $t0, $v0
+        add     $t0, $t0, $s5       # turn index into pointer
+        lb      $t1, 0($t0)         # dereference pointer
+        
+        bne     $t1, 1, skip        # skip pixel if not black
+        li      $t1, 2
+        sb      $t1, 0($t0)         # mark pixel as explored (code 2)
+        
+        beqz    $v0, noleft         # add left neighbor to queue
+        add     $a0, $v0, -1
+        move    $a1, $v1
+        jal     queue_push
+noleft: beqz    $v1, noup           # add upper neighbor
+        move    $a0, $v0
+        add     $a1, $v1, -1
+        jal     queue_push
+noup:   beq     $v0, 319, norigh    # add right neighbor
+        add     $a0, $v0, 1
+        move    $a1, $v1
+        jal     queue_push
+norigh: beq     $v1, 239, skip      # add bottom neighbor
+        move    $a0, $v0
+        add     $a1, $v1, 1
+        jal     queue_push
+        
+skip:   jal     queue_empty         # break if there are no more pixels to explore
+        beqz    $v0, exloop
         
         move    $a0, $s6            # restore arguments and return to caller
         move    $a1, $s5
