9d3a361aea6ef719d99a5250796947732187091e
Michał Szopiński <mszopinski@mssnt.pl>
2021-01-10 22:34:39 +0100

    Add BMP header verification

--- markerfinderx86/bmpreader.asm
+++ markerfinderx86/bmpreader.asm
@@ -5,5 +5,39 @@ SECTION .text
         GLOBAL read_bitmap
         
 read_bitmap:
-        mov eax, 8
+        push        ebp                     ; create new stack frame
+        mov         ebp, esp
+        push        ebx                     ; save registers
+        push        esi
+        push        edi
+        
+        mov         esi, DWORD [ebp + 8]    ; set pointer to start of buffer
+        
+        mov         ax, WORD [esi]          ; verify BMP header
+        cmp         ax, 0x4D42
+        jne         fail2
+        mov         eax, DWORD [esi + 14]   ; verify header size
+        cmp         eax, 40
+        jne         fail3
+        mov         eax, DWORD [esi + 30]   ; verify compression
+        cmp         eax, 0
+        jne         fail3
+        mov         ax, WORD [esi + 28]     ; verify bit depth
+        cmp         ax, 24
+        jne         fail4
+        
+        mov         eax, 7
+        jmp         exit
+        
+fail2:  mov         eax, -2                 ; labels for failing with the given code
+        jmp         exit
+fail3:  mov         eax, -3
+        jmp         exit
+fail4:  mov         eax, -4
+        
+exit:   pop         edi                     ; restore registers
+        pop         esi
+        pop         ebx
+        mov         esp, ebp                ; restore old stack frame
+        pop         ebp
         ret
