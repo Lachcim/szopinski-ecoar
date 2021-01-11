b2c16e62ae4507235cc280c6a194566f93a2558a
Michał Szopiński <mszopinski@mssnt.pl>
2021-01-11 23:17:53 +0100

    Add width detection

--- markerfinderx86/validator.asm
+++ markerfinderx86/validator.asm
@@ -8,9 +8,30 @@ SECTION .text
 validate_marker:
         push        ebp                     ; create new stack frame
         mov         ebp, esp
+        push        esi                     ; preserve registers
+        sub         esp, 8                  ; local variables
         
-        mov         eax, 1
+        mov         ecx, 0                  ; get marker width
+        mov         esi, [ebp + 8]          ; move start pointer to esi
+
+.width: inc         ecx                     ; increment counter and pointer
+        inc         esi
+        mov         al, [esi]               ; dereference pointer
+        cmp         al, 1                   ; iterate until white encountered
+        je          .width
+        mov         DWORD [esp + 4], ecx    ; save width
         
+        jmp         .ok
+        
+.fail:  mov         eax, [esp + 4]          ; return -width
+        neg         eax
+        jmp         .exit
+        
+.ok:    mov         eax, [esp + 4]          ; return (width - 1)
+        dec         eax
+
+.exit:  add         esp, 8                  ; pop local variables
+        pop         esi                     ; restore registers
         mov         esp, ebp                ; restore old stack frame
         pop         ebp
         ret
