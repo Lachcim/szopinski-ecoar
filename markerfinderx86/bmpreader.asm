2686468b01586afc19d3b3908834402e944b805f
Michał Szopiński <mszopinski@mssnt.pl>
2021-01-11 01:56:34 +0100

    Implement BMP reader

--- markerfinderx86/bmpreader.asm
+++ markerfinderx86/bmpreader.asm
@@ -26,7 +26,44 @@ read_bitmap:
         cmp         ax, 24
         jne         fail4
         
-        mov         eax, 7
+        push        DWORD [esi + 18]        ; image width
+        push        DWORD [esi + 22]        ; line counter initialized to height
+        mov         ecx, DWORD [esp + 4]    ; column counter initialized to width
+        
+        mov         eax, 322                ; set destination pointer to lower left corner
+        mul         DWORD [esi + 22]        ; plus a margin of one column and one row
+        add         eax, 1
+        mov         edi, DWORD [ebp + 12]   ; initialize as start of buffer and add offset
+        add         edi, eax
+        add         esi, 54                 ; move source pointer to first pixel
+        
+        mov         eax, DWORD [esp + 4]    ; calculate padding bytes as width mod 3
+        and         eax, 3
+        
+paint:  cmp         BYTE [esi], 0           ; check any RGB component for a nonzero value
+        jnz         pcont
+        cmp         BYTE [esi + 1], 0
+        jnz         pcont
+        cmp         BYTE [esi + 2], 0
+        jnz         pcont
+
+        mov         [edi], BYTE 1           ; if pixel is all black, mark as 1 in bitmap
+
+pcont:  add         esi, 3                  ; increment src and dst pointers
+        inc         edi
+        loop        paint                   ; loop until end of line reached
+        
+        sub         edi, 322                ; reset dst pointer to start of previous line
+        sub         edi, DWORD [esp + 4]
+        mov         ecx, DWORD [esp + 4]    ; reset column counter to image width
+        add         esi, eax                ; skip padding bytes
+        
+        sub         DWORD [esp], 1          ; decrement line counter
+        cmp         DWORD [esp], 0          ; loop until top line reached
+        jnz         paint
+        
+        sub         esp, 8                  ; pop width and line counter
+        mov         eax, 0                  ; return zero on success
         jmp         exit
         
 fail2:  mov         eax, -2                 ; labels for failing with the given code
